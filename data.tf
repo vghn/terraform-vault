29662292d450c584c87ca4b84f547bf54dc05fcc
Vlad <vlad@ghn.me>
2020-03-21 08:46:23 -0500

    Add EBS volume

--- data.tf
+++ data.tf
@@ -1,3 +1,26 @@
+# Vault Data Volume
+resource "aws_ebs_volume" "vault_data" {
+  availability_zone = "us-east-1a"
+  type              = "gp2"
+  encrypted         = true
+  size              = 2
+
+  tags = merge(
+    var.common_tags,
+    {
+      "Name"     = "Vault Data"
+      "Snapshot" = "true"
+    },
+  )
+}
+
+resource "aws_volume_attachment" "vault_data_attachment" {
+  device_name  = "/dev/sdg"
+  instance_id  = aws_instance.vault.id
+  volume_id    = aws_ebs_volume.vault.id
+  skip_destroy = true
+}
+
 # Vault HA DynamoDB table
 resource "aws_dynamodb_table" "vault" {
   name           = "vault"
